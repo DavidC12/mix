c04c6b05b726ec7f764e9d22dd890131dce8bc77
Dave Clough <d_clough@hotmail.com>
2018-12-19 05:15:15 -0500

    Properties infrastructure (static only) with test property added

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,18 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 /**
  * This class only exists for annotations.  It has no logic.
  * @author d_clo
  */
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @ComponentScan("com.dc.mixdb")
-public class AppConfig {}
+@PropertySource("app.properties")
+public class AppConfig {
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
--- src/main/java/com/dc/mixdb/repository/AlbumRepository.java
+++ src/main/java/com/dc/mixdb/repository/AlbumRepository.java
@@ -3,6 +3,7 @@ package com.dc.mixdb.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.dc.mixdb.enumeration.Constants;
@@ -13,9 +14,14 @@ import com.dc.mixdb.model.IAlbum;
 @Repository(Constants.ALBUM_REPOSITORY)
 public class AlbumRepository implements IAlbumRepository {
 	
+	@Value("${eyeCatcher}")
+	String eyeCatcher;
 	
 	@Override
 	public List<IAlbum> findAll() {
+		
+		System.out.println("eyeCatcher = " + eyeCatcher);
+		
 		List<IAlbum> albumsList = new ArrayList<>();
 		
 		AlbumCompilation ac = new AlbumCompilation();
